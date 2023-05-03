cd6165dd99e01890e24c8de10340b92d560a678a
viniciustakedi <viniciustakedi7@gmail.com>
2023-05-03 17:48:18 -0300

    Chore: reorganize file

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main className='teste'>
+    <main className='main'>
       <nav>
         <h1>Menu</h1>
       </nav>
-      <div className='main'>
+      <div className='content'>
         <h1>Home</h1>
         <p>Count: {count}</p>
         <button onClick={() => setCount(state => state + 1)}>Teste</button>
